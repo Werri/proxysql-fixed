c3416c97103fae358bc1e934eac77cf2d0a1a225
Werri <werri@bitcrystal.de>
2019-02-24 23:10:45 +0100

    ok

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-killall -9 proxysql;rm /var/lib/proxysql.db;cd src;rm proxysql;cd ..;make;cd src;./proxysql;./proxysql;mysql -uroot -proot -P 6033 -h 127.0.0.1 mysql
+killall -9 proxysql;rm /var/lib/proxysql.db;cd src;rm ./proxysql;cd ..;make;cd src;./proxysql;./proxysql;mysql -uroot -proot -P 6033 -h 127.0.0.1 mysql
